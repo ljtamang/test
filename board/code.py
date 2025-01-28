7a5b4ed6c954cb10e90667fbd2bae7a2d53297e9
Lasang Jimba Tamang <zimbabiru@gmail.com>
2025-01-28 17:52:28 -0600

    Update code.py

--- board/code.py
+++ board/code.py
@@ -1,9 +1,10 @@
 SELECT 
     SPLIT_PART(file_relative_path, '/', 2) AS product_name,
-    file_relative_path,
-    file_extension,
-    category,
-    CONCAT('https://github.com/department-of-veterans-affairs/va.gov-team/blob/main/', file_relative_path) AS github_link
+    CONCAT('=HYPERLINK("https://github.com/department-of-veterans-affairs/va.gov-team/blob/master/', 
+           file_relative_path,
+           '","',
+           file_relative_path,
+           '")') AS file_path
 FROM 
     your_table_name 
 ORDER BY 
