2c0b5967041fe3eb5c7b9410d2c81b233c14471e
Lasang Jimba Tamang <zimbabiru@gmail.com>
2025-01-28 17:23:52 -0600

    Update code.py

--- board/code.py
+++ board/code.py
@@ -1,27 +1,8 @@
-import os
-import zipfile
-from pyspark.dbutils import DBUtils
-
-# Path to the source folder in DBFS
-source_folder = "/dbfs/tmp/destination-folder"
-
-# Create a temporary zip file
-zip_path = "/dbfs/tmp/download_folder.zip"
-
-# Create a zip file containing the folder contents
-def zip_folder(folder_path, zip_path):
-    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                arcname = os.path.relpath(file_path, folder_path)
-                zipf.write(file_path, arcname)
-
-# Zip the folder
-zip_folder(source_folder, zip_path)
-
-# Create a link to download the zip file
-displayHTML(f"""<a href="/files{zip_path.replace('/dbfs', '')}" download>Download Folder as ZIP</a>""")
-
-# Optional: Clean up the zip file after some time
-# dbutils.fs.rm(zip_path)
+SELECT
+    -- Extract the second part of the file_relative_path as product_name
+    SPLIT_PART(file_relative_path, '/', 2) AS product_name,
+    file_relative_path,
+    file_extension,
+    category
+FROM
+    your_table_name
