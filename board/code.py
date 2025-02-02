03622473eded3db6406a06c451080b1141ac51f3
Lasang Jimba Tamang <zimbabiru@gmail.com>
2025-02-02 12:25:56 -0600

    Update code.py

--- board/code.py
+++ board/code.py
@@ -1,8 +1,2 @@
-import pandas as pd
-import numpy as np
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-import re
-import nltk
-from textblob import TextBlob  # For sentiment analysis
+# Filter rows where 'trust' and 'comments' columns are not empty
+df_comments = df[(df['trust'].notna()) & (df['comments'].notna())].copy()
