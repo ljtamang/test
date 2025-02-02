896b6a2f13a5d95f241d28018fe41b7bf4288899
Lasang Jimba Tamang <zimbabiru@gmail.com>
2025-02-02 13:27:03 -0600

    Update code.py

--- board/code.py
+++ board/code.py
@@ -1,4 +1,1 @@
-# Add sentiment classification column using a lambda function
-df_comments['sentiment'] = df_comments['sentiment_score'].apply(
-    lambda score: "Positive" if score > 0 else ("Negative" if score < 0 else "Neutral")
-)
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
