fe937adf51e49f66af31d505bfcecaa12ffba6a7
Lasang Jimba Tamang <zimbabiru@gmail.com>
2025-02-02 13:30:03 -0600

    Update code.py

--- board/code.py
+++ board/code.py
@@ -1,1 +1,5 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+df['sentiment'] = df['comment'].apply(lambda x: SentimentIntensityAnalyzer().polarity_scores(x)['compound'])
